b9326901720e15b6bdf8f4b6d1abc29c27e526d2
berenice bourgon <berenice.bourgon@gmail.com>
2019-02-06 18:10:30 +0100

    rectif des controlelers

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
 
 resources :gossips
 resources :cities
+resources :users
 
 root 'gossips#index'
 
 get '/team', to: 'thegossipcontroller#team'
 get '/contact', to: 'thegossipcontroller#contact'
 
-get 'author/:id', to: 'thegossipcontroller#author_page'
+#get 'author/:id', to: 'thegossipcontroller#author_page'
 
 
 
